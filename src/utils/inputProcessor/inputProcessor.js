5779f966df3fa26543c5e6b4ff487d4238ea519f
Jamie Allen <Jamieallen59@gmail.com>
2016-11-12 10:17:58 +0000

    Add comments to the input processor

--- src/utils/inputProcessor/inputProcessor.js
+++ src/utils/inputProcessor/inputProcessor.js
@@ -11,17 +11,18 @@ export const dirtPatchInputProcessor = inputs => {
 
 	const formattedInputs = []
 
+	// Maps through all the dirt patches given and puts them in their own array
+	// which is then places in a parent array
 	inputs.map((input, index) => {
 		const shouldCreateNewArray = isMultipleOfTwo(index)
-		// Depending on first or second pair or the coord pairs
-		// Either:
+		// Depending whether it is the first or second element of the coord pairs either:
 		// 1) Create a new array with the first coord
 		if (shouldCreateNewArray) {
 			const newArray = [ input ]
 
 			return formattedInputs.push(newArray)
 		}
-		// 2) Add the second coord to the latest inner array to form a pair
+		// 2) Add the second coord to the latest inner array to form the pair
 		const targetIndex = formattedInputs.length - 1
 
 		return formattedInputs[targetIndex].push(input)
@@ -34,17 +35,24 @@ export const parsedArrayToInts = array => (
 	array.map(element => parseInt(element, 10))
 )
 
+// Takes in a string as input and:
 export default input => {
 	if (typeof input !== 'string') {
 		throw new Error('The input type must be a string')
 	}
-
+	// removes surrounding whitespace and splits by spaces
 	const formattedInputs = input.trim().split(/\s+/)
+	// takes the final input as the hoovers instructions
 	const lastInput = formattedInputs.pop()
+	// splits the hoovers instructions by letter resulting in an array of letters
 	const instructions = lastInput.split('')
+	// turns the remaining inputs into an array of numbers
 	const inputsParsedToInts = parsedArrayToInts(formattedInputs)
+	// takes the first two inputs as the room dimensions
 	const roomDimensions = inputsParsedToInts.slice(0, 2)
+	// takes the first third and fourth inputs as the hoovers position
 	const hooverPosition = inputsParsedToInts.slice(2, 4)
+	// takes the remaining elements as the patches of dirt
 	const remainingElements = inputsParsedToInts.slice(4)
 	const dirtyPatches = dirtPatchInputProcessor(remainingElements)
 
