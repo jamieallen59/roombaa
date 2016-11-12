1c175915e3e1e2de6e00c9cc8b2785a381c90874
Jamie Allen <Jamieallen59@gmail.com>
2016-11-12 11:48:06 +0000

    Added express to serve site

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const renderFile = require('ejs').renderFile
+const app = express()
+
+app.engine('html', renderFile)
+app.set('view engine', 'html')
+
+const staticPath = 'dist'
+app.use(express.static(staticPath))
+
+app.listen(3000)
